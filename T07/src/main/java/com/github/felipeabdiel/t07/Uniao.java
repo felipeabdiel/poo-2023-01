7392f951153515630fc2efea679e17d87df3f836
vitorbielb <vitor.life18092004@gmail.com>
2023-06-04 12:14:08 -0300

    Atualização de Repositório

---
+++ T07/src/main/java/com/github/felipeabdiel/t07/Uniao.java
@@ -0,0 +1,15 @@
+package com.github.felipeabdiel.t07;
+
+public class Uniao {
+    private ArrayList filhos;
+    private Pessoa parceiros[2];
+
+    public void novoFilho(Pessoa p){
+        filhos.add(p);
+    }
+
+    public Uniao(Pessoa a, Pessoa b){
+        parceiros[0] = a;
+        parceiros[1] = b;
+    }
+}
