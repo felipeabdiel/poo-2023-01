52772c7e4be5c3970dbab70ce8d30ee77b1ce317
felipeabdiel <felipe_brito@discente.ufg.br>
2023-06-04 17:28:39 -0300

    Atualização de Repositório

---
+++ T07/src/main/java/com/github/felipeabdiel/t07/Referencia.java
@@ -0,0 +1,7 @@
+package com.github.felipeabdiel.t07;
+
+public class Referencia {
+}
+
+class Revista extends Referencia{
+}
---
+++ T07/src/main/java/com/github/felipeabdiel/t07/Religiao.java
@@ -0,0 +1,9 @@
+package com.github.felipeabdiel.t07;
+
+public class Religiao {
+    private Arraylist devotos;
+
+    public void novoFiel(Devoto d){
+        devotos.add(d);
+    }
+}
