2222af746546a8c72f1e6933d8d7a18b75acb854
Felipe Brito Gonçalves <109626398+felipeabdiel@users.noreply.github.com>
2023-06-20 20:35:41 -0300

    Delete T08/src/main/java/com/github/felipeabdiel/t08/território directory

--- T08/src/main/java/com/github/felipeabdiel/t08/território/Cidade.java
+++
@@ -1,6 +0,0 @@
-package main.java.com.github.felipeabdiel.t08.território;
-
-public class Cidade {
-    boolean capital;
-    String nome;
-}
--- T08/src/main/java/com/github/felipeabdiel/t08/território/Pais.java
+++
@@ -1,7 +0,0 @@
-package main.java.com.github.felipeabdiel.t08.território;
-
-public class Pais {
-    String nome;
-    int qtdCidades;
-    Cidade capital;
-}
