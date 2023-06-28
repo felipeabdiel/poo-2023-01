cc161f3462ab36f855c1ba84b5dbfadfd900df8e
Felipe Brito Gonçalves <felipe_brito@discente.ufg.br>
2023-06-27 21:57:25 -0300

    Atualização de Repositório

---
+++ T09/src/main/java/com/github/felipeabdiel/t09/circle/Circulo.java
@@ -0,0 +1,10 @@
+package main.java.com.github.felipeabdiel.t09.circle;
+
+public class Circulo {
+    float x, y, raio, dx, dy;
+
+    public void method(float dx, float dy) {
+        this.x = dx;
+        this.y = dy;
+    }
+}
---
+++ T09/src/main/java/com/github/felipeabdiel/t09/person/Pessoa.java
@@ -0,0 +1,7 @@
+package main.java.com.github.felipeabdiel.t09.person;
+
+public class Pessoa {
+    String nome;
+    Habito habitos;
+    int idade;
+}
