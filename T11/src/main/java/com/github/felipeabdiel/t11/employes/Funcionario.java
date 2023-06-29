337971373020745c254e1b4a3a12fb66b494f1d2
Felipe Brito Gonçalves <felipe_brito@discente.ufg.br>
2023-06-29 18:14:08 -0300

    Atualização de Repositório

---
+++ T11/src/main/java/com/github/felipeabdiel/t11/employes/Funcionario.java
@@ -0,0 +1,6 @@
+package main.java.com.github.felipeabdiel.t11.employes;
+
+public class Funcionario {
+    boolean gerente;
+    String nome;
+}
---
+++ T11/src/main/java/com/github/felipeabdiel/t11/employes/Lanchonete.java
@@ -0,0 +1,5 @@
+package main.java.com.github.felipeabdiel.t11.employes;
+
+public class Lanchonete {
+    Funcionario funcionarios[];
+}
