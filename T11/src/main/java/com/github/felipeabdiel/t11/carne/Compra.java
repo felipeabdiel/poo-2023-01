72ffd30c46f1769e8899938ca6aa0b4f719b65a7
Felipe Brito Gonçalves <felipe_brito@discente.ufg.br>
2023-06-29 17:34:12 -0300

    Atualização de Repositório

---
+++ T11/src/main/java/com/github/felipeabdiel/t11/carne/Compra.java
@@ -0,0 +1,6 @@
+package main.java.com.github.felipeabdiel.t11.carne;
+
+public class Compra {
+    ItemCompra compra;
+    Carne carne;
+}
---
+++ T11/src/main/java/com/github/felipeabdiel/t11/carne/Produto.java
@@ -0,0 +1,5 @@
+package main.java.com.github.felipeabdiel.t11.carne;
+
+public class Produto {
+
+}
