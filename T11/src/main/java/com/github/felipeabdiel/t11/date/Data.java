138d24abe478cdbba6204cdb1337d27e41116069
Felipe Brito Gonçalves <felipe_brito@discente.ufg.br>
2023-06-29 18:03:12 -0300

    Atualização de Repositório

---
+++ T11/src/main/java/com/github/felipeabdiel/t11/date/Data.java
@@ -0,0 +1,5 @@
+package main.java.com.github.felipeabdiel.t11.date;
+
+public class Data {
+    String data;
+}
---
+++ T11/src/main/java/com/github/felipeabdiel/t11/date/Periodo.java
@@ -0,0 +1,5 @@
+package main.java.com.github.felipeabdiel.t11.date;
+
+public class Periodo {
+    Data dataInicio, dataFinal;
+}
