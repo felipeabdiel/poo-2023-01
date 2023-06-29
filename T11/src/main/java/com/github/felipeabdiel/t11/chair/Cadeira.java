c95f6baf9cace0ce1e7d47b211ad33e6bd3b21a3
Felipe Brito Gonçalves <felipe_brito@discente.ufg.br>
2023-06-29 15:57:46 -0300

    Atualização Chair T11

---
+++ T11/src/main/java/com/github/felipeabdiel/t11/chair/Cadeira.java
@@ -0,0 +1,4 @@
+package main.java.com.github.felipeabdiel.t11.chair;
+
+public class Cadeira {
+}
---
+++ T11/src/main/java/com/github/felipeabdiel/t11/chair/Pessoa.java
@@ -0,0 +1,6 @@
+package main.java.com.github.felipeabdiel.t11.chair;
+
+public class Pessoa {
+    Cadeira cadeira; // do qual é dono
+    String nome;
+}
