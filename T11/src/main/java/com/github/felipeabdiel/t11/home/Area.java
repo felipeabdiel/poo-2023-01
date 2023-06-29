743b457786e3f5e866b21fb3210a2dd11617fcf6
Felipe Brito Gonçalves <felipe_brito@discente.ufg.br>
2023-06-29 15:50:31 -0300

    Atualização Home T11

---
+++ T11/src/main/java/com/github/felipeabdiel/t11/home/Area.java
@@ -0,0 +1,5 @@
+package main.java.com.github.felipeabdiel.t11.home;
+
+public class Area {
+
+}
---
+++ T11/src/main/java/com/github/felipeabdiel/t11/home/Casa.java
@@ -0,0 +1,9 @@
+package main.java.com.github.felipeabdiel.t11.home;
+
+public class Casa {
+    Garagem garagem;
+    Banheiro banheiro;
+    Sala sala;
+    Quarto quarto;
+    Area area;
+}
