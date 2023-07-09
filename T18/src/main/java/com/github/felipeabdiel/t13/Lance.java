324e64f00595a5ee489ac1bfbc8dfb59bdd6cf86
Felipe Brito Gonçalves <felipe_brito@discente.ufg.br>
2023-07-09 18:22:00 -0300

    Atualização T18

--- T18/src/main/java/com/github/felipeabdiel/t13/Lance.java
+++ T18/src/main/java/com/github/felipeabdiel/t13/Lance.java
@@ -1,7 +1,13 @@
 package main.java.com.github.felipeabdiel.t13;
 
 public class Lance {
-    Posicao origem, destino;
+    Posicao destino;
     Peca peca;
     Jogador jogador;
+
+    public Lance(Peca peca, Posicao destino, Jogador jogador) {
+        this.destino = destino;
+        this.peca = peca;
+        this.jogador = jogador;
+    }
 }
